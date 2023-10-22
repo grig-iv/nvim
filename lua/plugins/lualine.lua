f118c86c9c55083cc6b5d26d6950b8e164858259
grig-iv <abstractgrig@gmail.com>
2023-10-22 14:52:31 +0300

    add lauline

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,33 @@
+-- TODO syncthing status integration
+return {
+    "nvim-lualine/lualine.nvim",
+    event = { 'BufReadPost', 'BufNewFile' },
+    opts = {
+        options = {
+            section_separators = { left = '', right = '' },
+            component_separators = '|',
+        },
+        sections = {
+            lualine_a = { 'mode' },
+            lualine_b = { 'branch' },
+            lualine_c = { {
+                'filename',
+                path = 1,
+                newfile_status = true,
+                symbols = {
+                    modified = '󰧞',
+                    readonly = '󰌾',
+                    unnamed = '[No Name]',
+                    newfile = '[New]',
+                }
+            } },
+            lualine_x = { 'diagnostics' },
+            lualine_y = { 'encoding' },
+            lualine_z = { 'location' }
+        },
+    },
+    config = function(_, opts)
+        require("lualine").setup(opts)
+        vim.opt.showmode = false -- disable "--INSERT--" notification in command line
+    end,
+}
