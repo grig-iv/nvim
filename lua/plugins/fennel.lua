932c730c31f41d7b814ab50bebf9bff8d0ac20b6
grig-iv <abstractgrig@gmail.com>
2024-01-06 02:13:28 +0300

    minor changes

--- lua/plugins/fennel.lua
+++ lua/plugins/fennel.lua
@@ -1,14 +1,14 @@
 return {
     {
         'jaawerth/fennel.vim',
-        ft = "fennel",
+        ft = 'fennel',
         config = function()
             local function set_hl(name, val)
                 vim.api.nvim_set_hl(0, name, val)
             end
 
-            set_hl("FennelSpecialForm", { link = "Keyword" })
-            set_hl("FennelKeyword", { link = "@lsp.type.parameter" })
+            set_hl('FennelSpecialForm', { link = 'Keyword' })
+            set_hl('FennelKeyword', { link = '@lsp.type.parameter' })
         end
     },
 }
