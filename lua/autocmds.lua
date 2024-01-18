c8691df2697a9028a3625474c368bd137ef0f2f9
grig-iv <abstractgrig@gmail.com>
2024-01-18 23:23:45 +0300

    fix fost locuse error

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,13 +1,17 @@
 local vim = _G.vim
 
-vim.api.nvim_create_autocmd("TextYankPost", {
-    pattern = "*",
+vim.api.nvim_create_autocmd('TextYankPost', {
+    pattern = '*',
     callback = function() vim.highlight.on_yank() end,
-    desc = "Highlight on yank"
+    desc = 'Highlight on yank'
 })
 
-vim.api.nvim_create_autocmd("FocusLost", {
-    pattern = "*",
-    callback = function() vim.api.nvim_command(":wa") end,
-    desc = "Highlight on yank"
+vim.api.nvim_create_autocmd('FocusLost', {
+    pattern = '*',
+    callback = function()
+        if vim.fn.bufname('%') ~= '' and vim.api.nvim_buf_get_option(0, 'buftype') == '' then
+            vim.api.nvim_command(':wa')
+        end
+    end,
+    desc = 'Highlight on yank'
 })
