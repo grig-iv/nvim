f71fdbda9f926633ce72d93ae4b1f74f316f8bd0
grig-iv <abstractgrig@gmail.com>
2024-02-10 12:20:13 +0300

    fix luasnip

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -2,16 +2,23 @@ return {
     'hrsh7th/nvim-cmp', -- Autocompletion plugin
     event = { 'InsertEnter', 'CmdlineEnter' },
     dependencies = {
-        'hrsh7th/cmp-nvim-lsp',   -- Autocompletion with LSPs
-        'hrsh7th/cmp-buffer',     -- Autocompletion from words in buffer
-        'hrsh7th/cmp-path',       -- Autocompletion from files
-        'hrsh7th/cmp-cmdline',    -- Autocompletion for nvim commands
+        'hrsh7th/cmp-nvim-lsp',     -- Autocompletion with LSPs
+        'hrsh7th/cmp-buffer',       -- Autocompletion from words in buffer
+        'hrsh7th/cmp-path',         -- Autocompletion from files
+        'hrsh7th/cmp-cmdline',      -- Autocompletion for nvim commands
 
-        "PaterJason/cmp-conjure", --conjure
+        'PaterJason/cmp-conjure',   -- conjure
+        'L3MON4D3/LuaSnip',         -- lua snip
+        'saadparwaiz1/cmp_luasnip', -- lua snip
     },
     config = function()
         local cmp = require('cmp')
         cmp.setup({
+            snippet = {
+                expand = function(args)
+                    require('luasnip').lsp_expand(args.body)
+                end,
+            },
             mapping = cmp.mapping.preset.insert({
                 ['<Down>'] = cmp.mapping.select_next_item(),
                 ['<Up>'] = cmp.mapping.select_prev_item(),
@@ -21,10 +28,10 @@ return {
                 { name = 'nvim_lsp' },
                 { name = 'buffer',  keyword_length = 4 },
                 {
-                    name = "buffer",
+                    name = 'buffer',
                     option = {
                         sources = {
-                            { name = "conjure" },
+                            { name = 'conjure' },
                         },
                     },
                 },
