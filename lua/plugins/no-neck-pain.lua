2c1c6199199f6d90035534861b266c116ba4ef24
grig-iv <abstractgrig@gmail.com>
2024-02-10 12:20:23 +0300

    add no-neck-pain

---
+++ lua/plugins/no-neck-pain.lua
@@ -0,0 +1,16 @@
+return {
+    'shortcuts/no-neck-pain.nvim',
+    version = '*',
+    config = function(_, opts)
+        local no_neck_pain = require('no-neck-pain')
+
+        no_neck_pain.setup(opts)
+
+        vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
+            pattern = { '*.norg', '*.md' },
+            callback = function()
+                vim.schedule(no_neck_pain.enable)
+            end,
+        })
+    end
+}
