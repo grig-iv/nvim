d0b0e4a53b8305fa3a230d25af31b267fcad74bd
grig-iv <abstractgrig@gmail.com>
2024-02-26 21:12:06 +0300

    small changes

--- lua/plugins/local-highlight.lua
+++ lua/plugins/local-highlight.lua
@@ -1,5 +1,8 @@
 return {
-    "tzachar/local-highlight.nvim",
-    event = "BufReadPost",
-    opts = { hlgroup = "Underlined" }
+    'tzachar/local-highlight.nvim',
+    event = 'BufReadPost',
+    opts = {
+        hlgroup = 'Underlined',
+        disable_file_types = { 'norg' }, -- FIXME dosen't work
+    }
 }
