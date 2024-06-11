130b4db74da2c14d6578a972d16f4c043ccbb9ae
grig-iv <abstractgrig@gmail.com>
2024-06-11 19:44:24 +0000

    yazi: change plugin

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -1,10 +1,13 @@
 return {
-    'DreamMaoMao/yazi.nvim',
+    'mikavilpas/yazi.nvim',
     dependencies = {
-        'nvim-telescope/telescope.nvim',
         'nvim-lua/plenary.nvim',
     },
+    event = 'VeryLazy',
     keys = {
-        { '<C-e>', '<cmd>Yazi<CR>', desc = 'Toggle Yazi', },
+        { '<C-e>', function() require('yazi').yazi() end, desc = 'Open the file manager', },
+    },
+    opts = {
+        yazi_floating_window_border = 'none',
     },
 }
