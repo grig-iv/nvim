2e1c0e85767c476c17ba09a1a7f75ee51cf60d77
grig-iv <abstractgrig@gmail.com>
2024-09-30 20:53:09 +0300

    update

--- lua/plugins/no-neck-pain.lua
+++ lua/plugins/no-neck-pain.lua
@@ -9,6 +9,11 @@ return {
         vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter', }, {
             pattern = { '*.norg', '*.md', },
             callback = function()
+                local buf_name = vim.api.nvim_buf_get_name(0)
+                if buf_name == '/tmp/scratchpad.md' then
+                    return
+                end
+
                 vim.schedule(no_neck_pain.enable)
             end,
         })
