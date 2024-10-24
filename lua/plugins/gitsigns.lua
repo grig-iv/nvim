0e3f658269be4601a506ff83c2bbbe0b9d2f5a1f
grig-iv <abstractgrig@gmail.com>
2024-10-24 23:27:31 +0300

    add gitsigns bindings

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -5,6 +5,15 @@ return {
             if vim.fn.count(vim.api.nvim_buf_get_name(buf), '/Extended Mind/') > 0 then
                 return false
             end
+
+            local gitsigns = require('gitsigns')
+            local map = require('utils').map
+
+            local opts = { buffer = buf, }
+
+            map('n', '<leader>hp', gitsigns.preview_hunk, opts)
+            map('n', '<leader>hr', gitsigns.reset_hunk, opts)
+            map('n', '<leader>ho', gitsigns.show, opts)
         end,
     },
 }
