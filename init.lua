0c1c5070133b24ac447b5aca798abd018ae37f40
grig-iv <abstractgrig@gmail.com>
2026-01-19 22:37:46 +0300

    add prettier for json

--- init.lua
+++ init.lua
@@ -374,6 +374,8 @@ require('lazy').setup {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        json = { 'prettier' },
+        jsonc = { 'prettier' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
